8e44da9b2d8327edd048b40500390a192e17250e
Jaypal Sapara <48550828+jaypalsapara@users.noreply.github.com>
2026-07-22 13:19:37 +0530

    fix(shadcn): Fixed useFlashToast() in toaster component

--- resources/js/components/ui/sonner.tsx
+++ resources/js/components/ui/sonner.tsx
@@ -1,9 +1,12 @@
 import { useTheme } from "next-themes"
 import { Toaster as Sonner, type ToasterProps } from "sonner"
 import { CircleCheckIcon, InfoIcon, TriangleAlertIcon, OctagonXIcon, Loader2Icon } from "lucide-react"
+import { useFlashToast } from "@/hooks/use-flash-toast";
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
+
+  useFlashToast();
 
   return (
     <Sonner
